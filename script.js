3eb00a4bcfe7f32c2fa4fe23b25aa74ec62624a5
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-09 04:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let max = -Infinity, result = -Infinity;
+
+  for (const value of arr) {
+    const nr = Number(value)
+
+    if (nr > max) {
+      [result, max] = [max, nr] // save previous max
+    } else if (nr < max && nr > result) {
+      result = nr; // new second biggest
+    }
+  }
+
+  return result;
